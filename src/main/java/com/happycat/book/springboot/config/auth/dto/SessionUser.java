1324ceb4355dae0c4f7e15f06384479bfd6f39d5
jaemin <pjaemin@gmail.com>
2022-08-02 02:54:04 +0900

    구글 로그인 추가 웹 보안 정책 추가 로그인유저 리팩토링 세션db 추가

---
+++ src/main/java/com/happycat/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.happycat.book.springboot.config.auth.dto;
+
+import com.happycat.book.springboot.domain.user.Person;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(Person user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
---
+++ src/main/java/com/happycat/book/springboot/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.happycat.book.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
+}
