9a8ef886af8c4e8c1d3aa996de57bf17a150e587
jaemin <pjaemin@gmail.com>
2022-08-04 02:14:38 +0900

    코드 개선

--- src/test/java/com/happycat/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/happycat/book/springboot/web/HelloControllerTest.java
@@ -1,20 +1,27 @@
 package com.happycat.book.springboot.web;
 
+import com.happycat.book.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
-@WebMvcTest
+@WebMvcTest(controllers=HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes= SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -24,6 +31,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name="hello";
         int amount = 10000;
